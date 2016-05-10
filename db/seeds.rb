5aac1f77e4f91fe9b600dde39ada6ef39966f665
John Doe <max.s32@i.ua>
2016-05-10 23:12:01 +0300

    Add seeds. Add stylesheets for product table. Modify product table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(title: 'Prtogramming ruby 1.9 & 2.0',
+                description: 
+                %{<p>
+                  Ruby is the fastest growing and most exciting dynamic language,
+                  out there. If you need to get working programs delivered fast,
+                  you should add Ruby to your toolbox.
+                 <p>},
+                image_url: 'ruby.jpg',
+                price: 49.95) 
+# . . .
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description:
+                %{<p>
+                  Ruby is the fastest growing and most exciting dynamic language
+                  out there. If you need to get working programs delivered fast,
+                  you should add Ruby to your toolbox.
+                </p>},
+                  image_url: 'ruby.jpg',
+                  price: 49.95)
+# . . .
+Product.create!(title: 'Rails Test Prescriptions',
+                description: 
+                %{<p>
+                  <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+                  Rails applications, covering Test-Driven Development from both a
+                  theoretical perspective (why to test) and from a practical perspective
+                  (how to test effectively). It covers the core Rails testing tools and
+                  procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+                  including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+                </p>},
+                   image_url: 'rtp.jpg',
+                   price: 34.95)
